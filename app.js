b95161e06890dd12b5b7601e06d9a34e60e51bec
Salla <salla.tero@gmail.com>
2019-03-29 12:22:01 +0200

    4.2 Omat middlewaret omassa moduulissa, lisätty jsconfig.json

--- app.js
+++ app.js
@@ -5,18 +5,28 @@ const bodyParser = require('body-parser')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
 const cors = require('cors')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 //Yhdistetään tietokantaan
 console.log('connecting to db at ', config.MONGODB_URI)
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
-//lisää then ja catch
+.then(() => {
+    console.log('connected to MongoDB')
+})
+.catch((error) => {
+    console.log('error connecting to MongoDB: ', error.message)  
+})
 
 //Otetaan middlewareja käyttöön
+//app.use(express.static('build'))
 app.use(cors())
 app.use(bodyParser.json())
+app.use(middleware.requestLogger)
 
 //Otetaan blogsRouter käyttöön ja käytetään sitä vain jos polun alku on /api/blogs
 app.use('/api/blogs', blogsRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
