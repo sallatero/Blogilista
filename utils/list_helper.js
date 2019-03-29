f903233098a5d74a47ad91e2527289d5d62f61c2
Salla <salla.tero@gmail.com>
2019-03-29 19:03:15 +0200

    4.5 favoriteBlog-testi

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -11,7 +11,29 @@ const totalLikes = (blogs) => {
   return totalLikes
 }
 
+//Etsii sen blogin jolla on eniten likejä
+const favoriteBlog = (blogs) => {
+  let traceInitial = {
+    title: '',
+    _id: '',
+    author: '',
+    likes: 0
+  }
+  const favoriteBlog = blogs.reduce((trace, blog) => {
+    if (blog.likes > trace.likes) {
+      trace.title = blog.title
+      trace.likes = blog.likes
+      trace.author = blog.author
+      trace._id = blog._id
+    }
+    return trace
+  }, traceInitial)
+  console.log(`blog with most likes: ${JSON.stringify(favoriteBlog)}`)
+  return favoriteBlog
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
