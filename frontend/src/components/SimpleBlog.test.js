bd2ae028b01129a43d19bd8516490edf16d8956d
Salla <salla.tero@gmail.com>
2019-04-15 20:48:35 +0300

    jest, lint ongelmia

---
+++ frontend/src/components/SimpleBlog.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import 'jest-dom/extend-expect'
+import { render, cleanup } from 'react-testing-library'
+import { prettyDOM } from 'dom-testing-library'
+import SimpleBlog from './SimpleBlog'
+
+afterEach(cleanup)
+
+test('renders content', () => {
+  const blog = {
+    title: 'Test title',
+    author: 'Test Author',
+    likes: 8
+  }
+  const mockHandler = jest.fn()
+
+  const component = render(<SimpleBlog blog={blog} onClick={mockHandler} />)
+
+  expect(component.container).toHaveTextContent('Test title')
+})
